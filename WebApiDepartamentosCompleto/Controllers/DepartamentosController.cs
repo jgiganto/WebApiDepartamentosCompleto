8abe156e4603d45e48ff602f1bb8ec2ab15d96e2
Jesús Giganto Diez <Jesus.Giganto@tajamar365.com>
2018-04-18 19:02:48 +0200

    a

---
+++ WebApiDepartamentosCompleto/Controllers/DepartamentosController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiDepartamentosCompleto.Models;
+
+namespace WebApiDepartamentosCompleto.Controllers
+{
+    public class DepartamentosController : ApiController
+    {
+        ModeloDepartamentos modelo;
+        public DepartamentosController()
+        {
+            modelo = new ModeloDepartamentos();
+        }
+        // GET: api/Departamentos
+        public List<DEPT> Get()
+        {
+            return modelo.GetDepartamentos();
+        }
+
+        // GET: api/Departamentos/5
+        public DEPT Get(int id)
+        {
+            return modelo.BuscarDepartamento(id);
+        }
+
+        // POST: api/Departamentos
+        // POST: api/Departamentos/11/NOMBRE/LOCALIDAD
+        //void Post(int num, String nom, String loc)
+        //INSERTAR DEPARTAMENTO
+        public void Post(DEPT dept)
+        {
+            modelo.InsertarDepartamento(dept.DEPT_NO, dept.DNOMBRE
+                , dept.LOC);        }
+
+
+        // PUT: api/Departamentos/5
+        //MODIFICAR DEPARTAMENTO
+        public void Put(int id, DEPT dept)
+        {
+            modelo.ModificarDepartamento(id, dept.DNOMBRE
+                , dept.LOC);
+        }
+
+        // DELETE: api/Departamentos/5
+        public void Delete(int id)
+        {
+            modelo.BorrarDepartamento(id);
+        }
+
+    }
+}
---
+++ WebApiDepartamentosCompleto/Models/ModeloDepartamentos.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiDepartamentosCompleto.Models
+{
+    public class ModeloDepartamentos
+    {
+        ContextoDepartamentos contexto;
+
+        public ModeloDepartamentos()
+        {
+            this.contexto = new ContextoDepartamentos();
+        }
+        //GET()
+        public List<DEPT> GetDepartamentos()
+        {
+            var consulta = from datos in contexto.DEPT
+                           select datos;
+            return consulta.ToList();
+        }
+
+        //GET(id)
+        public DEPT BuscarDepartamento(int depno)
+        {
+            return contexto.DEPT.Find(depno);
+        }
+        //POST
+        public void InsertarDepartamento(int num, String nom,String loc)
+        {
+            DEPT dep = new DEPT();
+            dep.DEPT_NO = num;
+            dep.DNOMBRE = nom;
+            dep.LOC = loc;
+            contexto.DEPT.Add(dep);
+            contexto.SaveChanges();
+        }
+        //PUT()
+        public void ModificarDepartamento(int num,String nom, String loc)
+        {
+            DEPT dep = this.BuscarDepartamento(num);
+            dep.DNOMBRE = nom;
+            dep.LOC = loc;
+            contexto.SaveChanges();            
+        }
+        //DELETE()
+        public void BorrarDepartamento(int num)
+        {
+            DEPT dep = this.BuscarDepartamento(num);
+            contexto.DEPT.Remove(dep);
+            contexto.SaveChanges();
+        }
+
+    }
+}
